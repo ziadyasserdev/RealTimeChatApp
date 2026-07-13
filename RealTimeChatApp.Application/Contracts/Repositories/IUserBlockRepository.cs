bfbb5cc1e7d91496b379f30838449aa802e641a8
Ziad <ziadyasserabtelmiget@gmail.com>
2026-07-13 09:19:36 +0300

    Add Private  Messages Feature

---
+++ RealTimeChatApp.Application/Contracts/Repositories/IUserBlockRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeChatApp.Application.Contracts.Repositories
+{
+    internal class IUserBlockRepository
+    {
+    }
+}
