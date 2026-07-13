ca6096e5abbfec223a8c026ba681e48361181f69
Ziad <ziadyasserabtelmiget@gmail.com>
2026-07-13 10:30:54 +0300

    add forward private message to group

---
+++ RealTimeChatApp.Application/Features/PrivateMessages/Commands/ForwardPrivateMessageToGroup/ForwardPrivateMessageToGroupCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeChatApp.Application.Features.PrivateMessages.Commands.ForwardPrivateMessageToGroup
+{
+    internal class ForwardPrivateMessageToGroupCommand
+    {
+    }
+}
